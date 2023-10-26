6a4a8f26c11a473ef90745c75da105d2efbc261f
EslamSaber8 <islamsaper5@gmail.com>
2023-10-26 13:19:11 +0300

    commit 2

--- server.js
+++ server.js
@@ -1,42 +1,62 @@
-const mongoose = require('mongoose');
+const path = require('path');
+
+const express = require('express');
 const dotenv = require('dotenv');
+const morgan = require('morgan');
 
-process.on('uncaughtException', err => {
-  console.log('UNCAUGHT EXCEPTION! 💥 Shutting down...');
-  console.log(err.name, err.message,err.stack);
-  process.exit(1);
+dotenv.config({ path: 'config.env' });
+const ApiError = require('./utils/apiError');
+const globalError = require('./middlewares/errorMiddleware');
+const dbConnection = require('./config/database');
+// Routes
+const studentRoute = require('./routes/studentRoute');
+const studentAuthRoute = require('./routes/studentAuthRoute');
+const teacherRoute = require('./routes/teacherRoute');
+const teacherAuthRoute = require('./routes/teacherAuthRoute');
+const courseRoute=require("./routes/courseRoute")
+
+
+// Connect with db
+dbConnection();
+
+// express app
+const app = express();
+
+// Middlewares
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'uploads')));
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+  console.log(`mode: ${process.env.NODE_ENV}`);
+}
+
+// Mount Routes
+app.use('/api/v1/students', studentRoute);
+app.use('/api/v1/studentAuth', studentAuthRoute);
+app.use('/api/v1/teachers',teacherRoute);
+app.use('/api/v1/teacherAuth', teacherAuthRoute);
+app.use('/api/v1/course', courseRoute);
+
+
+
+app.all('*', (req, res, next) => {
+  next(new ApiError(`Can't find this route: ${req.originalUrl}`, 400));
 });
 
-dotenv.config({ path: './config.env' });
-const app = require('./app');
+// Global error handling middleware for express
+app.use(globalError);
 
-const DB = process.env.DATABASE.replace(
-  '<PASSWORD>',
-  process.env.DATABASE_PASSWORD
-);
-
-mongoose
-  .connect(DB, {})
-  .then(() => console.log('DB connection successful!'));
-
-
-
-
-
-
-
-
-
-
-const port = process.env.PORT || 9000;
-const server = app.listen(port, () => {
-  console.log(`App running on port ${port}...`);
+const PORT = process.env.PORT || 9000;
+const server = app.listen(PORT, () => {
+  console.log(`App running running on port ${PORT}`);
 });
 
-process.on('unhandledRejection', err => {
-  console.log('UNHANDLED REJECTION! 💥 Shutting down...');
-  console.log(err.name, err.message,err.stack);
+// Handle rejection outside express
+process.on('unhandledRejection', (err) => {
+  console.error(`UnhandledRejection Errors: ${err.name} | ${err.message}`);
   server.close(() => {
+    console.error(`Shutting down....`);
     process.exit(1);
   });
 });
